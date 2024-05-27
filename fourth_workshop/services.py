bff56a49b3cad10f0d3d520ba2599b768661afb3
anunezb <allweneedislove1984@gmail.com>
2024-05-27 18:46:13 -0500

    workshop 4: update code

--- fourth_workshop/services.py
+++ fourth_workshop/services.py
@@ -1,3 +1,5 @@
+"""This modyule contains the code for the FastAPI web service."""
+
 import uvicorn
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
@@ -16,33 +18,47 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/hello_ud")
 def hello_ud():
+    """This function return a welcome message."""
     return "Welcome to UD!"
 
-engine = create_engine('postgresql://postgres:postgres@localhost:5432/public')
+
+engine = create_engine("postgresql://postgres:postgres@localhost:5432/public")
 Session = sessionmaker(bind=engine)
 session = Session()
 
 metadata = MetaData()
-products = Table('products', metadata,
-                 Column('id', Integer, primary_key=True),
-                 Column('name', String),
-                 Column('description', String))
+products = Table(
+    "products",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String),
+    Column("description", String),
+)
+
 
 @app.get("/get_products")
 def get_products():
+    """This function returns the list of products."""
     query = products.select()
     result = session.execute(query)
-    product_list = result.fetchall()  # Cambiado el nombre de la variable a 'product_list' para evitar la confusión con la tabla 'products'
+    # Cambiado el nombre de la variable a 'product_list' para evitar confusión con tabla 'products'
+    product_list = result.fetchall()
     return product_list
 
-@app.post("/create_products") # Web services cannot be named the same because this could be an error
+
+@app.post(
+    "/create_products"
+)  # Web services cannot be named the same because this could be an error
 def create_product(name: str, description: str):
+    """This function creates a new product."""
     query = products.insert().values(name=name, description=description)
     session.execute(query)
     session.commit()
     return {"message": "Product created successfully"}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8002)  # Cambiado el puerto a 8002
